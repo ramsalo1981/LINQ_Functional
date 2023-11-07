a4a0f00a9ce3a668baa88fb59bd1867776294395
ramsalo1981 <rami.salloum@hotmail.com>
2023-11-07 23:42:07 +0100

    first commit

---
+++ CADelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CADelegate
+{
+    internal class Program
+    {
+        //delegate void MDelegate();
+        //Action, Func, Pred
+        static void Main(string[] args)
+        {
+            //M2(M1);
+            Action action = M1;
+            M2(action);
+            Console.ReadKey();
+        }
+        static void M1()
+        {
+
+            Console.WriteLine("M1");
+        }
+
+        static void M2(/*MDelegate mdel*/ Action action)
+        {
+            //mdel();
+            action();
+            Console.WriteLine("M2");
+        }
+    }
+}
